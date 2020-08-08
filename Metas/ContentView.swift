2ec89fc6c311c95cf2a6b889717f19ddbff1465d
JohnKostanov <kostanovd@icloud.com>
2020-08-08 18:03:15 +0200

    Initial Commit

---
+++ Metas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Metas
+//
+//  Created by  Джон Костанов on 08/08/2020.
+//  Copyright © 2020 John Kostanov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
